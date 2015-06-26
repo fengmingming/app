6511381fbd0bebae9a6f0452a2cd16256c9cc3b2
冯明明 <1053931671@qq.com>
2015-06-26 18:05:36 +0800

    new project

---
+++ app/src/main/java/com/app/BottomFragment.java
@@ -0,0 +1,20 @@
+package com.app;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by sls-30 on 2015/6/26.
+ */
+public class BottomFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.bottom,container,false);
+    }
+}
