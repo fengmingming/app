99529d1543545a767cdb740cf3085060c75d081f
冯明明 <1053931671@qq.com>
2015-07-03 00:50:53 +0800

    loop images

--- app/src/main/java/com/app/MainActivityFragment.java
+++ app/src/main/java/com/app/MainActivityFragment.java
@@ -5,18 +5,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageSwitcher;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
 
+    private ImageSwitcher imageS;
+
     public MainActivityFragment() {
     }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.container, container, false);
+        View view = inflater.inflate(R.layout.container, container, false);
+        imageS = (ImageSwitcher) getActivity().findViewById(R.id.index_imageswitcher);
+        return view;
     }
 }
